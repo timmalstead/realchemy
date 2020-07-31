6dc4aa92aeebd603b91033c06f8675eed1184979
Tim Malstead <timmmalstead@gmail.com>
2020-07-30 18:58:31 -0700

    further refines drawing logic

--- src/types/props.d.ts
+++ src/types/props.d.ts
@@ -1,13 +1,15 @@
 import { MutableRefObject } from "react"
 
+type ReactCanvasRefObject = MutableRefObject<HTMLCanvasElement>
+
 export interface appToCanvas {
-  canvasRef: MutableRefObject<HTMLCanvasElement>
+  canvasRef: ReactCanvasRefObject
   width: number
   height: number
 }
 
 export interface drawingLogic {
-  canvasRef: MutableRefObject<HTMLCanvasElement>
+  canvasRef: ReactCanvasRefObject
   context: CanvasRenderingContext2D | null
   setContext: any
 }
